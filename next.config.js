eb55b82d0f89749954409baa3d438ae035195eb4
2041gustavo <gustavoreis2040@hotmail.com>
2025-02-24 08:57:28 -0300

    terceiro commit

--- next.config.js
+++ next.config.js
@@ -4,8 +4,12 @@ const nextConfig = {
   swcMinify: true,
   env: {
     NODE_ENV: process.env.NODE_ENV
-  },
-  telemetry: false
+  }
+}
+
+// Desabilitar telemetria externamente
+if (process.env.NEXT_TELEMETRY_DISABLED !== '1') {
+  process.env.NEXT_TELEMETRY_DISABLED = '1';
 }
 
 module.exports = nextConfig 
